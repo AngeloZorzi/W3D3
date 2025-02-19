a16f82fa452522c394596b6c69fe12ca233dc0f2
AngeloZorzi <astro.drg666@gmail.com>
2025-02-19 17:42:05 +0100

    Update W3D3.js

--- W3D3.js
+++ W3D3.js
@@ -9,7 +9,7 @@ pageForm.addEventListener("submit", (e) => {
   console.log("lista", toDoList);
   const card = document.createElement("div");
   card.classList.add("cardToDo");
-  card.innerHTML = `<p onclick="done()" id = "inlinep">${toDoList.toDo}
+  card.innerHTML = `<p onclick="done()" id = "inlinep">${toDoList.toDo} </p>
   <button onclick="deletecard()">ELIMINA</button>
   `;
   const conteiner = document.getElementById("savedList");
@@ -20,3 +20,4 @@ const done = function () {
   const par = document.getElementById("inlinep");
   par.classList.add("pline");
 };
+//tutto sbagliato scusa Stefano! lo rifaccio con calma riguardando la lezione...
